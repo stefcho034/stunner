ac7f5fabb33cb20e8a51615b51c3021b000f9021
Stefan Lazarov <stefan.lazarov@kongsbergdigital.com>
2022-11-21 18:02:02 +0200

    Update 3

--- deploy/install.ps1
+++ deploy/install.ps1
@@ -1,6 +1,6 @@
 $chart = Join-Path -Path $PSScriptRoot -ChildPath ".."
 
-helm repo add stunner https://l7mp.io/stunner
+helm repo add stunner https://stefcho034.github.io/stunner
 helm repo update
 
 helm dependencies build $chart
